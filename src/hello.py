6f9fc560dbc7d7f127ddce004dea3aee35cfa42e
Oscar Fridh <oscar.fridh.mkk@gmail.com>
2022-03-02 16:59:14 +0100

    Added code, without docs - MISS

--- src/hello.py
+++ src/hello.py
@@ -5,3 +5,10 @@ def hello(name = None, preferred_language="en"):
         print(f"Hej, {name}!")
     else:
         print(f"Hello, {name}!")
+
+
+def fib(n):
+   if n <= 1:
+       return n
+   else:
+       return(fib(n-1) + fib(n-2))
